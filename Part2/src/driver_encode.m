3a721e27c62524e46640751297ee1a44c0777138
Paras Jain <parasjain@parasjain.com>
2015-03-30 18:52:34 -0400

    driver

--- Part2/src/driver_encode.m
+++ Part2/src/driver_encode.m
@@ -7,6 +7,8 @@ y1 = mod(a1 * x', 2);
 y0'
 y1'
 
-y  = create_encoded_word(y0, y1)
+y            = create_encoded_word(y0, y1)
 
-[x_1, x_2] = decode_word(y)
+[x_1, x_2]   = decode_word(y)
+[a_0, a_1]   = create_transformation_matrix(5)
+
