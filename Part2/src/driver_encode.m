2be3a596dd768bebe0b19b9ba3e6a7048d66e400
Neil B. Patel <neilb.patel5@gmail.com>
2015-03-30 18:58:50 -0400

    Revert "driver"
    This reverts commit 3a721e27c62524e46640751297ee1a44c0777138.

--- Part2/src/driver_encode.m
+++ Part2/src/driver_encode.m
@@ -7,8 +7,6 @@ y1 = mod(a1 * x', 2);
 y0'
 y1'
 
-y            = create_encoded_word(y0, y1)
+y  = create_encoded_word(y0, y1)
 
-[x_1, x_2]   = decode_word(y)
-[a_0, a_1]   = create_transformation_matrix(5)
-
+[x_1, x_2] = decode_word(y)
