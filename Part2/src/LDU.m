ff724c9559489ed2ee9ad57c6a28212e805c32a9
Faizan Virani <fvirani6@gatech.edu>
2015-03-30 19:48:37 -0400

    Almost finished LDU

---
+++ Part2/src/LDU.m
@@ -0,0 +1,37 @@
+function [L,D,U] = LDU( A )
+[row, col] = size(A);
+y = 1;
+L = zeros(row,col);
+while y<=col-1
+   x = y + 1;
+   temp = zeros(row, 1);
+   while x <= row
+       temp(x,1) = A(x,y);
+       x = x+1;
+   end
+   L(:,y) = temp;
+   y = y+1;
+end
+
+y = 1;
+D = zeros(row,col);
+while y <= col
+    x = y;
+    D(x,y) = A(x,y);
+    x = x+1;
+    y = y+1;
+end
+
+U = zeros(row,col);
+x = 1
+while x<=row-1
+   y = x + 1;
+   temp = zeros(1, col);
+   while y <= col
+       temp(1,y) = A(x,y);
+       y = y+1;
+   end
+   U(x,:) = temp;
+   x = x+1;
+end
+
