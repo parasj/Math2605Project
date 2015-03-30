e4c915e32074344a28a4471e641fe033da3cd8f0
Paras Jain <parasjain@parasjain.com>
2015-03-30 18:29:58 -0400

    driver

--- Part2/src/create_encoded_word.m
+++ Part2/src/create_encoded_word.m
@@ -1,7 +1,7 @@
 function [ y ] = create_encoded_word( x1, x2 )
-    [~, n] = size(x1);
+    [n, ~] = size(x1);
     y = zeros(1, n);
     for i = 1 : n
-        y(i) = x1(i) * 10 + x2(i);
+        y(i) = x1(i) * 2 + x2(i);
     end
 end
---
+++ Part2/src/driver.m
@@ -0,0 +1,10 @@
+x = [1 0 1 1 0];
+[a0, a1] = create_transformation_matrix(5)
+
+y0 = mod(a0 * x', 2);
+y1 = mod(a1 * x', 2);
+
+y0'
+y1'
+
+y  = create_encoded_word(y0, y1)
