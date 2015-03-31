e443b6036d092c437fd1be5809cd692716fd7164
Neil B. Patel <neilb.patel5@gmail.com>
2015-03-30 20:27:50 -0400

    Jacobi Updated

--- Part2/src/jacobi.m
+++ Part2/src/jacobi.m
@@ -1,7 +1,19 @@
 function [its] = jacobi(A, x0, tol)
-b = A[:,end];
-A(:,end) = [];
+    b = A[:,end];
+    A(:,end) = [];
+    its = 0;
 
-
+    delta = x0;
+    while ((delta > tol) && (its <= 100))
+        [L, D, U] = LDU(A);
+        S = D;
+        T = -1 * L - U;
+        xn = matrix_mult(S', matrix_mult(T, xn)) + matrix_mult(S', b);
+    end
+    if iterations > 101
+       error('Did not converge in 100 iterations');
+    end
+    else
+        print ('Converged 
 end
 
