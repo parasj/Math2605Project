d740aa2c412534706772193dd24353f3d110c308
Paras Jain <parasjain@parasjain.com>
2015-03-31 19:02:07 -0400

    square matrix

--- Part2/src/create_transformation_matrix.m
+++ Part2/src/create_transformation_matrix.m
@@ -1,6 +1,6 @@
 function [ A0, A1 ] = create_transformation_matrix( n )
-    A0 = zeros(n + 3, n);
-    A1 = zeros(n + 3, n);
+    A0 = zeros(n + 3, n + 3);
+    A1 = zeros(n + 3, n + 3);
     for i = 1 : n
         A1(i:i+3, i) = [1 1 0 1];
         A0(i:i+3, i) = [1 0 1 1];
