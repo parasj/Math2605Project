7b7887d9caf806f81b303624ad7da00e128c0b1d
Paras Jain <parasjain@parasjain.com>
2015-03-31 18:45:50 -0400

    drivers for part 2

--- Part2/src/decode_word.m
+++ Part2/src/decode_word.m
@@ -3,7 +3,7 @@ function [ x1, x2 ] = decode_word( y )
     x1 = zeros(1, n);
     x2 = zeros(1, n);
     for i = 1 : n
-        x1(i) = bitand(y(i), 2) * 1/2;
         x2(i) = bitand(y(i), 1);
+        x1(i) = int8((y(i) - x2(i)) / 10);
     end
 end
--- Part2/src/driver_decode.m
+++ Part2/src/driver_decode.m
@@ -1,2 +1,20 @@
-[y_1, y_2]   = decode_word(y)
-[a_0, a_1]   = create_transformation_matrix(5)
+[filename, pathname] = uigetfile('*.dat', 'Decode: Select a y vector to load');
+fileloc = strcat(pathname, filename);
+y = importdata(fileloc);
+
+[~, n] = size(y);
+
+[y0, y1]   = decode_word(y)
+[a0, a1]   = create_transformation_matrix(n)
+
+% a0 * x0 = y0
+x0 = [y0 0 0 0]' \ a0
+
+% a1 * x1 = y1
+x1 = [y1 0 0 0]' \ a1
+
+if (x0 ~= x1)
+    error('x mismatch');
+end
+
+x = x0
