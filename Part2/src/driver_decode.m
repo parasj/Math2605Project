eb37c0223bcc8563c594f8239a3a911583fae2d0
Paras Jain <parasjain@parasjain.com>
2015-03-31 18:52:28 -0400

    part 2

--- Part2/src/driver_decode.m
+++ Part2/src/driver_decode.m
@@ -7,14 +7,24 @@ y = importdata(fileloc);
 [y0, y1]   = decode_word(y)
 [a0, a1]   = create_transformation_matrix(n)
 
-% a0 * x0 = y0
-x0 = [y0 0 0 0]' \ a0
 
-% a1 * x1 = y1
-x1 = [y1 0 0 0]' \ a1
+key = questdlg( 'Which method should be to solve Ax=b?', 'Decode', 'Jacobi', 'Gauss-Seidel', 'Gauss-Seidel');
+   
+    switch key
+        case 'Jacobi'
+            % a0 * x0 = y0
+            x0 = [y0 0 0 0]' \ a0
+            % a1 * x1 = y1
+            x1 = [y1 0 0 0]' \ a1
+        case 'Gauss-Seidel'
+            % a0 * x0 = y0
+            x0 = [y0 0 0 0]' \ a0
+            % a1 * x1 = y1
+            x1 = [y1 0 0 0]' \ a1
+    end
 
 if (x0 ~= x1)
-    error('x mismatch');
+    error('X0 != X1 - some error occurred');
 end
 
 x = x0
