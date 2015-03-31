a4651fb2fcd0863811828818193ea2e7a7b7ccef
Neil B. Patel <neilb.patel5@gmail.com>
2015-03-31 15:16:09 -0400

    Inverse Works?

--- Part2/src/jacobi.m
+++ Part2/src/jacobi.m
@@ -1,12 +1,20 @@
 function [its] = jacobi(A, x0, tol)
     b = A[:,end];
     A(:,end) = [];
+    [~, col] = size(A);
     its = 0;
 
     delta = x0;
     while ((delta > tol) && (its <= 100))
         [L, D, U] = LDU(A);
         S = D;
+        y = 1;
+        while y <= col
+            x = y;
+            S(x,y) = 1/D(x,y);
+            x = x+1;
+            y = y+1;
+        end
         T = -1 * L - U;
         oldxn = xn;
         xn = matrix_mult(S, matrix_mult(T, xn)) + matrix_mult(S, b);
