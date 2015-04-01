a546d24a8e21096d12d6b0354b3e86082179aecd
Neil B. Patel <neilb.patel5@gmail.com>
2015-03-31 20:20:18 -0400

    GS and J Drivers

---
+++ Part2/src/driver_jacobi.m
@@ -0,0 +1,20 @@
+[filename, pathname] = uigetfile('*.dat', 'Jacobi: Select an n x (n + 1) matrix to load');
+fileloc = strcat(pathname, filename);
+A = importdata(fileloc);
+[filename2, pathname2] = uigetfile('*.dat', 'Jacobi: Select an n x 1 vector to load');
+fileloc2 = strcat(pathname2, filename2);
+vec = importdata(fileloc2);
+input = inputdlg('Enter tol, the tolerance of the Jacobi method', 'Tolerance');
+tol = str2num(input{1});
+
+[m, n] = size(A);
+
+if (m < 1 || n < 1 || m ~= (n - 1))
+    error('Malformed matrix, load an n x (n + 1) matrix.');
+end
+[m2,n2] = size(vec);
+if (m2 ~= m)
+    error('Incorrect dimensions for initial vector');
+end
+
+[iterations, solution] =  jacobi(A, vec, tol);
