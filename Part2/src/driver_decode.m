0c2af05e1a5b74bb77b5b4b6caccdd6e57a4e9dc
Paras Jain <parasjain@parasjain.com>
2015-03-31 20:01:21 -0400

    sync

--- Part2/src/driver_decode.m
+++ Part2/src/driver_decode.m
@@ -16,18 +16,15 @@ y_1 = [y1 0 0 0]';
 switch key
     case 'Jacobi'
         % a0 * x0 = y0
-        [iter0, x0] = jacobi([a0 y_0], zeros(n + 3, 1), .9)
-        % x0 = y_0 \ a0
-        
+        [iter0, x0] = jacobi([a0 y_0], zeros(n + 3, 1), .2);
         % a1 * x1 = y1
-        [iter1, x1] = jacobi([a1 y_1], zeros(n + 3, 1), .9)
-        % x1 = [y1 0 0 0]' \ a1
+        [iter1, x1] = jacobi([a1 y_1], zeros(n + 3, 1), .2);
         
     case 'Gauss-Seidel'
         % a0 * x0 = y0
-        x0 = [y0 0 0 0]' \ a0
+        [iter0, x0] = gauss_seidel([a0 y_0], zeros(n + 3, 1), .2);
         % a1 * x1 = y1
-        x1 = [y1 0 0 0]' \ a1
+        [iter1, x1] = gauss_seidel([a1 y_1], zeros(n + 3, 1), .2);
 end
 
 if (x0 ~= x1)
--- Part2/src/driver_encode.m
+++ Part2/src/driver_encode.m
@@ -3,9 +3,10 @@ input = inputdlg('Enter n, the size of the input stream', 'Encoding Driver');
 n = str2num(input{1});
 
 x = generate_rand_binary_stream(n);
+x_0 = [x 0 0 0];
 [a0, a1] = create_transformation_matrix(n)
-y0 = mod(matrix_mult(a0, x'), 2);
-y1 = mod(matrix_mult(a1, x'), 2);
-y = mod(create_encoded_word(y0, y1), 2);
+y0 = mod(matrix_mult(a0, x_0'), 2);
+y1 = mod(matrix_mult(a1, x_0'), 2);
+y = create_encoded_word(y0, y1);
 
 h = msgbox({strcat('x: ', mat2str(x)), '',strcat('y: ', mat2str(y))}, 'Encoding Driver');
